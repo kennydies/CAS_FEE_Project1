b8d67455dcac6f974ed8c5d4a4b5e6db7ac374a2
kenny <claudio.h@ilnet.ch>
2019-06-22 12:30:07 +0200

    todo controller and routes setup

---
+++ controller/todosController.mjs
@@ -0,0 +1,20 @@
+export class TodosController {
+
+    async getTodos(req, res) {
+
+    };
+
+    async createTodo(req, res) {
+
+    };
+
+    async showTodo(req, res) {
+
+    };
+
+    async deleteTodo(req, res) {
+
+    };
+}
+
+export const todosController = new TodosController();
---
+++ routes/todoRoutes.mjs
@@ -0,0 +1,10 @@
+import express from 'express';
+const router = express.Router();
+import {todosController} from '../controller/todosController';
+
+router.get("/", todosController.getTodos.bind(todosController));
+router.post("/", todosController.createTodo.bind(todosController));
+router.get("/:id/", todosController.showTodo.bind(todosController));
+router.delete("/:id/", todosController.deleteTodo.bind(todosController));
+
+export const todoRoutes = router;
