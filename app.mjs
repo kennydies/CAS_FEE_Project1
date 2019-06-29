f14f0c0f8a3e1a76a58371670237ee0667042aa2
kenny <claudio.h@ilnet.ch>
2019-06-29 13:41:53 +0200

    added jwt and utils/securitiy.mjs

--- app.mjs
+++ app.mjs
@@ -1,9 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
+import jwt from 'express-jwt';
 
 // import {indexRoutes} from './routes/indexRoutes';
-// import {todoRoutes} from './routes/todoRoutes';
+import {todoRoutes} from './routes/todoRoutes';
 
 const app = express();
 
@@ -13,13 +14,19 @@ app.use(express.static(path.resolve('public/html')));
 app.use(express.static(path.resolve('public')));
 
 app.use(bodyParser.json());
+const jwtSecret = 'gcW&rY7kXN#FuR-az!LZ4=Z^s7xDWHEdP*mqshG4-+uE!g6fhr';
+
+app.set("jwt-secret", jwtSecret);
+app.set("jwt-sign", {expiresIn: "1d", audience: "self", issuer: "todo"});
+app.set("jwt-validate", {secret: jwtSecret, audience: "self", issuer: "todo"});
 
 app.get('/', function (req, res) {
-   res.sendFile('/html/index.html', {root: __dirname + '/public/'});
+   // res.sendFile('/html/index.html', {root: __dirname + '/public/'});
 });
 
 // app.use('/', indexRoutes);
-// app.use('/todos', todoRoutes);
+app.use(jwt( app.get('jwt-validate')));
+app.use('/todos', todoRoutes);
 
 app.use(function (err, req, res, next) {
     if(err.name === 'UnauthorizedError'){
